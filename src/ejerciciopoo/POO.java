c8083986d3c8dc649b426a936354f2e0fbb7c5d7
Estudiante <Estudiante@10.203.2.192>
2017-02-14 15:41:26 -0500

    ya funciona crea el patron con clases

---
+++ src/ejerciciopoo/POO.java
@@ -0,0 +1,16 @@
+package ejerciciopoo;
+
+import ch.aplu.turtle.Turtle;
+
+/**
+ *
+ * @author Equipo
+ */
+public class POO {
+   
+    public static void main(String[] args) {
+       Escena e = new Escena();
+       e.patron();
+    }
+}
+
